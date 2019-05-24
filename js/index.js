3b3f9a9e3009fdea7a7ae1a1e13e4f624140c6c2
jeff-gosselin <jgoz.email@gmail.com>
2019-05-24 12:13:36 -0400

    working on responsive menu

--- js/index.js
+++ js/index.js
@@ -6,17 +6,45 @@ links.forEach(link => {
 });
 //////////////////////////////////////////////////////////////////
 
+// Hamburger Menu
 
+const burger = document.querySelector('.hamburger');
+burger.addEventListener('click', (e) => {
+	console.log(e.target);
+})
+
+
+//////////////////////////////////////////////////////////////////
+
+// Arrow Button Scroll
 const arrow = document.querySelector('.arrow-down > a')
-console.log(arrow);
 
 arrow.addEventListener('click', (e) => {
-	console.log("YO!");
 	e.preventDefault();
 	document.querySelector('.how-content').scrollIntoView({
             behavior: 'smooth'
         });
 });
+///////////////////////////////////////////////////////////////////
 
+// Form Validation
+const form = document.querySelector('form');
 const input = document.querySelector('.domain');
-console.log(input);
+input.value = "";
+const validate = /^([a-z\d-\.]+)\.([a-z]){2,8}$/;
+
+const validation = (inputField, regex) => {
+	if (regex.test(inputField.value)) {
+		inputField.className = 'domain valid';
+		inputField.value = 'Submitted.'
+	} else {
+		inputField.className = 'domain invalid';
+		inputField.value = "";
+		inputField.placeholder = 'Invalid: (e.g. mydomain.com)'
+	};
+}
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	validation(input, validate)
+})
